7eeec24e4d05126bf3d6cb4d2062cc5b995f02f2
KT <tranlamkhanhtuong@gmail.com>
2020-09-22 09:55:24 +0700

    initial commit

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Configuration;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer");
+            var conf = new ConsumerConfig
+            {
+                GroupId = ConfigurationManager.AppSettings["GroupId"].Trim().ToString(),
+                BootstrapServers = ConfigurationManager.AppSettings["BootstrapServers"].Trim().ToString(),
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<string, string>(conf).Build())
+            {
+                c.Subscribe(ConfigurationManager.AppSettings["topic"].Trim().ToString());
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Partition {cr.Partition} - Key {cr.Key} - Offset '{cr.Offset}' Timestamp {cr.Message.Timestamp.UnixTimestampMs} - Consumed message '{cr.Value}' - Topic {cr.Topic} - at: '{cr.TopicPartitionOffset}' TopicPartition {cr.TopicPartition}");
+                            Thread.Sleep(1000);
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Producer");
+            var config = new ProducerConfig
+            {
+                BootstrapServers = ConfigurationManager.AppSettings["BootstrapServers"].Trim().ToString()
+            };
+            var topic = ConfigurationManager.AppSettings["topic"].Trim().ToString();
+
+            Console.Write("Total message: ");
+            var totalMsg = int.Parse(Console.ReadLine());
+         
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                #region old
+                //try
+                //{
+                //    var dr = await p.ProduceAsync("first_topic", new Message<string, string> { Key = "tuong", Value = Newtonsoft.Json.JsonConvert.SerializeObject(new { foo = "bar 2" }) });
+                //    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                //}
+                //catch (ProduceException<Null, string> e)
+                //{
+                //    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                //}
+                #endregion
+
+                int numProduced = 0;
+                //int numMessages = 10;
+                for (int i = 0; i < totalMsg; ++i)
+                {
+                    //var key = "key"+i.ToString();
+                    var val = Newtonsoft.Json.JsonConvert.SerializeObject(new { foo = "tuong: " + i.ToString() });
+
+                    Console.WriteLine($"Producing record: {val}");
+
+                    producer.Produce(topic, new Message<Null, string> {Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                    //Thread.Sleep(1000);
+                }
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                producer.Flush(TimeSpan.FromSeconds(10));
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
